00ad9e44da13202ea4dfe48d54b329d48d55e73d
zenlines <charlesloughin@gmail.com>
2017-06-15 20:59:43 -0400

    translated forEach into for loop

---
+++ for loops problem set/translate_forEach.js
@@ -0,0 +1,8 @@
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+var colors = ["red", "orange", "yellow", "green"];
+
+for (var i = 0; i < numbers.length; i++) {
+  if(numbers[i] % 3 === 0) {
+    console.log(numbers[i]);
+  }
+}
