c0f12b29b1d63bd85cca0d30aff5e1ec4bd3f7c9
zenlines <charlesloughin@gmail.com>
2017-06-15 21:28:31 -0400

    updates to to do app

--- to do app/todo.js
+++ to do app/todo.js
@@ -4,12 +4,35 @@ var input = prompt("What would you like to do?");
 
  while(input !== "quit") {
    if(input === "list") {
-     console.log(tasks);
+     listTasks();
    }
     else if(input === "new") {
-      var newTask = prompt("Enter new task");
-      tasks.push(newTask);
+      addTask();
+    }
+    else if(input === "delete") {
+      deleteTask();
     }
     input = prompt("What would you like to do?");
  }
  console.log("Goodbye!")
+
+
+
+function listTasks() {
+  console.log("**********");
+  tasks.forEach(function(task, index) {
+    console.log(index + ": " + task);
+  });
+  console.log("**********");
+}
+
+function addTask() {
+  var newTask = prompt("Enter new task");
+  tasks.push(newTask);
+}
+
+function deleteTask() {
+  var index = prompt("Enter index of task to delete");
+  tasks.splice(index, 1);
+  console.log("Deleted task at " + index);
+}
