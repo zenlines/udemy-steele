0bc87c5fb4b15de0790b1e7e0422d29348833720
zenlines <charlesloughin@gmail.com>
2017-06-16 22:48:07 -0400

    Fixed functions to accept arguments

--- array problem set/problemSet.js
+++ array problem set/problemSet.js
@@ -1,17 +1,15 @@
 //take array as argument, print elements of array in reverse order (do not alter order of actual array)
-var sampleArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-function printReverse() {
-  for (var i = sampleArray.length - 1; i >= 0; i--) {
+function printReverse(arr) {
+  for (var i = arr.length - 1; i >= 0; i--) {
     console.log(sampleArray[i]);
   }
 }
 
 //take array as argument, return true if all array elements are identical, false otherwise
-function isUniform() {
-  for (var i = 1; i < sampleArray.length; i++) {
-    var value = sampleArray[0];
-    if (sampleArray[i] !== value) {
+function isUniform(arr) {
+  var value = arr[0];
+  for (var i = 1; i < arr.length; i++) {
+    if (arr[i] !== value) {
       return false;
     }
     else {
@@ -21,20 +19,20 @@ function isUniform() {
 }
 
 //take array of numbers and return sum of all numbers in array
-function sumArray() {
+function sumArray(arr) {
   var sum = 0;
-  for (var i = 0; i < sampleArray.length; i++) {
-    sum += sampleArray[i];
+  for (var i = 0; i < arr.length; i++) {
+    sum += arr[i];
   }
   console.log(sum);
 }
 
 //take array of numbers and returns highest number in array
-function max() {
-  var highNum = sampleArray[0];
-  for (var i = 1; i < sampleArray.length; i++) {
-    if (sampleArray[i] > highNum) {
-      highNum = sampleArray[i];
+function max(arr) {
+  var highNum = arr[0];
+  for (var i = 1; i < arr.length; i++) {
+    if (arr[i] > highNum) {
+      highNum = arr[i];
     }
   }
   console.log(highNum);
