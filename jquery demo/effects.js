ccdb769562f673fd986ecff7ea6fa2874e9bc05a
zenlines <charlesloughin@gmail.com>
2017-06-21 22:55:29 -0400

    Update effects.js

--- jquery demo/effects.js
+++ jquery demo/effects.js
@@ -1,5 +1,5 @@
-// $("#fadeExButton").on("click", function(){
-//   $(".ex").fadeOut(1000, function(){
-//     $(this).remove();
-//   });
-// });
+$("#fadeExButton").on("click", function(){
+  $(".ex").fadeOut(1000, function(){
+    $(this).remove();
+  });
+});
