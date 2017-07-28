e0555a1f397d6cbfc1bf0ea2c9badb23a1471120
zenlines <charlesloughin@gmail.com>
2017-07-28 19:27:08 -0400

    add first node exercise, echo function

---
+++ node exercises/echo.js
@@ -0,0 +1,18 @@
+//must take two arg, a string and a number
+//should print string n number of times, where n is the 2nd argument the function takes
+
+
+// function echo(str, num) {
+//     for(var i = 0; i < num; i++) {
+//         console.log(str);
+//     }
+// }
+
+function echo(str, num) {
+    for(var i = 0; i < num; i++) {
+        console.log(str);
+    }
+}
+
+echo("apples", 5)
+echo("johnny!", 3)
